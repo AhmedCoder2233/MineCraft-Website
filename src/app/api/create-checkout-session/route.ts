fcb6af0762bd1a117a37e204e9bd5a8cf308f1be
Ahmed <ahmedmemon3344@gmail.com>
2025-03-02 20:02:33 +0500

    Minecraft

--- src/app/api/create-checkout-session/route.ts
+++ src/app/api/create-checkout-session/route.ts
@@ -62,7 +62,7 @@ export async function POST(request: Request) {
         },
       ],
       mode: "payment",
-      success_url: `${process.env.NEXT_PUBLIC_BASE_URL}/success?session_id={CHECKOUT_SESSION_ID}`,
+      success_url: `${process.env.NEXT_PUBLIC_BASE_URL}/success`,
       metadata: {
         username,
         email,
